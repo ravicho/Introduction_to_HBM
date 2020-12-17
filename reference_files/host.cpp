6e186a7d9eee269d7fe219e6c957dc82dca2761d
ravicho <44479645+ravicho@users.noreply.github.com>
2020-12-17 07:39:08 -0800

    Updated

--- reference_files/host.cpp
+++ reference_files/host.cpp
@@ -29,6 +29,17 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "host.hpp"
 
+static uint64_t get_duration_ns(const std::vector<cl::Event>& events) {
+    uint64_t duration = 0;
+    for (size_t i=0; i<events.size(); i++) {
+        uint64_t start, end;
+        events[i].getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_START, &start);
+        events[i].getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_END, &end);
+        duration += end - start;
+    }
+    return duration;
+}
+
 int main(int argc, char** argv)
 {
   if (argc != 5) {
@@ -39,24 +50,29 @@ int main(int argc, char** argv)
   std::string binaryFile = argv[1];
   bool addRandom = atoi (argv[3]);
   unsigned int krnl_loop = atoi (argv[4]);
-  if ((addRandom==1) && (krnl_loop<64)) krnl_loop=64; 
+//  if ((addRandom==1) && (krnl_loop<64)) krnl_loop=64; 
 
   long unsigned int vector_size_bytes = (long unsigned int)atoi (argv[2])<<20; // Converted to MB
 
   cl_int err;
   unsigned fileBufSize;
-  size_t numIter = 1; 
+  size_t numIter = 1;
 
   /* Reducing the data size for emulation mode */
   char *xcl_mode = getenv("XCL_EMULATION_MODE");
   if (xcl_mode != NULL) {
-      std::cout << "** operating in HW EMU ** we are changing some values given as args to make the emulation run shorter and faster!" 
+      std::cout << "** operating in emulation: we are changing some args values to make the emulation run shorter and faster!"
           << std::endl;
-     vector_size_bytes = 16 * 1024;
-//     addRandom=1;
-     krnl_loop=1;	
+     vector_size_bytes = 64 * 1024;
+     if (addRandom == 1 && krnl_loop < 16) {
+         /* if random, then need to force a largish number of repeats
+          * to make sure all the range have more chances to being updated
+          * and not create false mismatches; since this is not guaranteed
+          * we will still count the number of mismatches */
+         krnl_loop=16;
+     }
   }
-  
+
   long unsigned int total_data_size = vector_size_bytes/sizeof(int) ; // Convert to number of integer words
 
   printf("\n Total Data of %2.3f Mbytes to be written to global memory from host\n ", (float) vector_size_bytes/1024/1024);
@@ -68,7 +84,7 @@ int main(int argc, char** argv)
   std::vector<int,aligned_allocator<int>> source_hw_results(total_data_size);
 //  std::vector<int,aligned_allocator<int>> source_sw_results(total_data_size);
 
-  // Create the test data 
+  // Create the test data
   for(long unsigned int i = 0 ; i < total_data_size ; i++){
      source_in1[i] = rand() ;//% total_data_size;
      source_in2[i] = rand() ;//% total_data_size;
@@ -80,7 +96,7 @@ int main(int argc, char** argv)
 	
 // ------------------------------------------------------------------------------------
 // Step 1: Get All PLATFORMS, then search for Target_Platform_Vendor (CL_PLATFORM_VENDOR)
-//	   Search for Platform: Xilinx 
+//	   Search for Platform: Xilinx
 // Check if the current platform matches Target_Platform_Vendor
 // ------------------------------------------------------------------------------------	
   std::vector<cl::Device> devices = get_devices("Xilinx");
@@ -119,6 +135,7 @@ int main(int argc, char** argv)
   cl::Buffer buffer_in1[numIter];
   cl::Buffer buffer_in2[numIter];
   cl::Buffer buffer_output[numIter];
+  std::vector<cl::Event> events_enqueueTask(numIter);
 
   // Create events for read,compute and write
   std::vector<cl::Event> host_2_device_Wait;
@@ -152,33 +169,41 @@ int main(int argc, char** argv)
 	
     q.finish();
     auto kernel_start = std::chrono::high_resolution_clock::now();
-    OCL_CHECK(err, err = q.enqueueTask(krnl_vector_add));
+    OCL_CHECK(err, err = q.enqueueTask(krnl_vector_add,NULL,&events_enqueueTask[j]));
     q.finish();
     auto kernel_end = std::chrono::high_resolution_clock::now();
     kernel_time += std::chrono::duration<double>(kernel_end - kernel_start);
 	
     OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output[j]},CL_MIGRATE_MEM_OBJECT_HOST));
- 
+
   }
   q.finish();
 //  auto kernel_end = std::chrono::high_resolution_clock::now();
 //  kernel_time = std::chrono::duration<double>(kernel_end - kernel_start);
   kernel_time_in_sec = kernel_time.count();
   std::cout << "kernel_time_in_sec = " << kernel_time_in_sec << std::endl;
+  auto duration_ns = get_duration_ns(events_enqueueTask);
+  std::cout << "Duration using events profiling: " << duration_ns << " ns" << std::endl;
+
 
   // Compare the results of the Device to the simulation
-  bool match = true;
+  long unsigned int match_count = 0;
+  long unsigned int mismatch_count = 0;
   for (size_t i = 0 ; i < total_data_size ; i++){
+      // when addRandom is used, the
     int source_sw_results = source_in1[i] + source_in2[i];
     if (source_hw_results[i] != source_sw_results){
+        mismatch_count++;
+        if(mismatch_count<10) {
       std::cout << "Error: Result mismatch" << std::endl;
       std::cout << "i = " << i << " CPU result = " << source_sw_results
                 << " Device result = " << source_hw_results[i] << std::endl;
-      match = false;
-      break;
+        }
+   } else {
+       match_count++;
    }
   }
-	
+  std::cout << " match_count = " << match_count << " mismatch_count = " << mismatch_count << " total_data_size = " << total_data_size << std::endl;
 // ============================================================================
 // Step 3: Release Allocated Resources
 // ============================================================================
@@ -187,12 +212,15 @@ int main(int argc, char** argv)
   // Multiplying the actual data size by 4 because four buffers are being
   // used.
   result = vector_size_bytes * krnl_loop;
+  double bandwidth2 = double(result) / duration_ns;
   result /= 1024;               // to KB
   result /= 1024;               // to MB
   result /= 1024;               // to GB
 
   result /= kernel_time_in_sec; // to GBps
-  std::cout << "Throughput Achived = " << result << " GB/s" << std::endl;
-  std::cout << "TEST " << (match ? "PASSED" : "FAILED") << std::endl; 
+//  std::cout << "Throughput Achived = " << result << " GiB/s" << std::endl; // Kilo units as 1024
+  std::cout << "Throughput Achived = " << bandwidth2 << " GB/s" << std::endl; // Kilo units as 1000
+  bool match = addRandom ? match_count > (total_data_size / 2) : mismatch_count == 0;
+  std::cout << "TEST " << (match ? "PASSED" : "FAILED") << std::endl;
   return (match ? EXIT_SUCCESS : EXIT_FAILURE);
 }
--- reference_files/kernel.cpp
+++ reference_files/kernel.cpp
@@ -29,34 +29,36 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /*******************************************************************************
 Description:
-    HLS pragmas can be used to optimize the design : improve throughput, reduce latency and 
+    HLS pragmas can be used to optimize the design : improve throughput, reduce latency and
     device resource utilization of the resulting RTL code
-    This is vector addition example to demonstrate how HLS optimizations are used in kernel. 
+    This is vector addition example to demonstrate how HLS optimizations are used in kernel.
 *******************************************************************************/
 
 
+//#define BUFFER_SIZE 1024
+//#define VDATA_SIZE 16
+//typedef struct v_datatype { unsigned int data[VDATA_SIZE]; } v_dt;
+
 #include "kernel.h"
 #include <ap_int.h>
-#include <iostream>
 
 // TRIPCOUNT identifier
-const unsigned int c_loop = VDATA_SIZE;
-const unsigned int c_total = 600/VDATA_SIZE;
+const unsigned int c_total = 1024/VDATA_SIZE;
 
-unsigned int minRand(void) {
+
 // 32 bits LFSR with taps at 32,22,2,1, as per table 3 of XAPP 052
 // https://www.xilinx.com/support/documentation/application_notes/xapp052.pdf
-  static ap_uint<32> lfsr=0x12345678; // initialize to non-zero value!
+unsigned int next_lfsr32(ap_uint<32> lfsr_in) {
 
-  bool b_32 = lfsr.get_bit(32 - 32);
-  bool b_22 = lfsr.get_bit(32 - 22);
-  bool b_2 = lfsr.get_bit(32 - 2);
-  bool b_1 = lfsr.get_bit(32 - 1);
+  bool b_32 = lfsr_in.get_bit(32 - 32);
+  bool b_22 = lfsr_in.get_bit(32 - 22);
+  bool b_2 = lfsr_in.get_bit(32 - 2);
+  bool b_1 = lfsr_in.get_bit(32 - 1);
   bool new_bit = b_32 ^ b_22 ^ b_2 ^ b_1;
-  lfsr = lfsr >> 1;
-  lfsr.set_bit(31, new_bit);
+  ap_uint<32> lfsr_out = lfsr_in >> 1;
+  lfsr_out.set_bit(31, new_bit);
 
-  return lfsr.to_uint();
+  return lfsr_out.to_uint();
 }
 
 
@@ -67,8 +69,8 @@ unsigned int minRand(void) {
  * gen_mask(127) -> 63
  * gen_mask(128) -> 127
  * gen_mask(129) -> 127
- * 
- * we will use this mask to clear the higher bits of the PRNG so that what 
+ *
+ * we will use this mask to clear the higher bits of the PRNG so that what
  * is left if of the same magnitude of the size, but smaller
 */
 unsigned int gen_mask(unsigned int x) {
@@ -82,93 +84,104 @@ gmask:
 }
 
 
-
 /*
-    Vector Addition Kernel Implementation 
+    Vector Addition Kernel Implementation
     Arguments:
         in1   (input)     --> Input Vector1
         in2   (input)     --> Input Vector2
         out   (output)    --> Output Vector
         size  (input)     --> Size of Vector in Integer
 */
-
-extern "C" {
+extern "C"
 void vadd(
-    const v_dt *in1,               // Read-Only Vector 1
-    const v_dt *in2,               // Read-Only Vector 2
-    v_dt *out,                     // Output Result for ADD
-    const unsigned int size,       // Size in integer
-    const unsigned int num_times,  // Running the same kernel operations num_times
-    bool addRandom                 // Address Pattern is random
-    ) {
-#pragma HLS INTERFACE m_axi port = in1 offset = slave bundle = gmem0 latency = 300 num_read_outstanding = 64 max_read_burst_length=64 max_write_burst_length=64
-#pragma HLS INTERFACE m_axi port = in2 offset = slave bundle = gmem1 latency = 300 num_read_outstanding = 64 max_read_burst_length=64 max_write_burst_length=64
-#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem2 // latency = 64
+        const v_dt *in1,               // Read-Only Vector 1
+        const v_dt *in2,               // Read-Only Vector 2
+        v_dt *out,                     // Output Result for ADD
+        const unsigned int size,       // Size in integer
+        const unsigned int num_times,  // Running the same kernel operations num_times
+        bool addRandom                 // Address Pattern is random
+        ) {
+#pragma HLS INTERFACE m_axi port = in1 offset = slave bundle = gmem0 latency = 300 num_read_outstanding = 128 num_write_outstanding=1
+#pragma HLS INTERFACE m_axi port = in2 offset = slave bundle = gmem1 latency = 300 num_read_outstanding = 128 num_write_outstanding=1
+#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem2 latency = 50  num_write_outstanding= 128 num_read_outstanding= 1
 
-#pragma HLS INTERFACE s_axilite port = in1 bundle = control
-#pragma HLS INTERFACE s_axilite port = in2 bundle = control
-#pragma HLS INTERFACE s_axilite port = out bundle = control
+#pragma HLS INTERFACE s_axilite bundle = control port = in1
+#pragma HLS INTERFACE s_axilite bundle = control port = in2
+#pragma HLS INTERFACE s_axilite bundle = control port = out
 
-#pragma HLS INTERFACE s_axilite port = size bundle = control
-#pragma HLS INTERFACE s_axilite port = num_times bundle = control
-#pragma HLS INTERFACE s_axilite port = return bundle = control
+#pragma HLS INTERFACE s_axilite bundle = control port = size
+#pragma HLS INTERFACE s_axilite bundle = control port = num_times
+#pragma HLS INTERFACE s_axilite bundle = control port = addRandom
+#pragma HLS INTERFACE s_axilite bundle = control port = return
 
-  int in_index = 0;
-  unsigned int seed = 1;
-  //unsigned int vSize = ((size - 1) / VDATA_SIZE) + 1;
-  unsigned int vSize = size / VDATA_SIZE;
-  unsigned int mask = gen_mask(vSize);
+    unsigned int in_index=0;
+    // size is a multiple of 16, even 1024, VDATA_SIZE is power of 2
+    unsigned int vSize = size / VDATA_SIZE;
 
-  v_dt tmpIn1, tmpIn2;
-  v_dt tmpOutAdd;
+    unsigned int mask = gen_mask(vSize);
 
+    // we separate the code between random access and sequential access
+    // because the compile is able to infer burst in the sequential case
+    // but if we merge the 2 codes, the burst is not inferred 
+    if(addRandom==false) {
+        // sequential access
+    seqvops1:
+        for (unsigned int count = 0; count < num_times; count++) {
+            // Running same kernel operation num_times to keep the kernel busy for HBM
+            // bandwidth testing
+            #pragma HLS LOOP_TRIPCOUNT min = c_total max = c_total
+            //Per iteration of this loop perform vSize vector addition
+    seqvops2:
+            for (unsigned int i = 0; i < vSize; i++) {
+                #pragma HLS LOOP_TRIPCOUNT min = VDATA_SIZE max = VDATA_SIZE
+                in_index = i;
 
-// Running same kernel operation num_times to keep the kernel busy for HBM
-// bandwidth testing
-main_loop:
-  for (int count = 0; count < num_times; count++) {
-    #pragma HLS LOOP_TRIPCOUNT min = c_total max = c_total
-    if (addRandom)  {
-    //Per iteration of this loop perform vSize vector addition
-    Loop_rndm: for (int i = 0; i < vSize; i++) {
-      #pragma HLS LOOP_TRIPCOUNT min = c_loop max = c_loop
-      // mask is less than vSize by construct, so randvalue will be too
-      unsigned int randvalue = minRand() & mask;
-      unsigned int candidate = in_index + randvalue; // less than 2x vSize
-      // need to make sure it is strictly less than vSize
-      // note: don't need a modulo operator because of the above masking and derived constraints
-      unsigned int in_index_random = candidate < vSize ? candidate : candidate-vSize;
-      in_index = in_index_random ;
+                v_dt tmpIn1 = in1[in_index];
+                v_dt tmpIn2 = in2[in_index];
+                v_dt tmpOutAdd;
 
-      tmpIn1 = in1[in_index];
-      tmpIn2 = in2[in_index];
+    seqvops3: // will be unrolled because parent loop is pipelined
+                for (int k = 0; k < VDATA_SIZE; k++) {
+                    tmpOutAdd.data[k] = tmpIn1.data[k] + tmpIn2.data[k];
+                }
 
-      vector_rndm:
-        for (int k = 0; k < VDATA_SIZE; k++) {
-          tmpOutAdd.data[k] = tmpIn1.data[k] + tmpIn2.data[k];
+                out[in_index] = tmpOutAdd;
+            }
         }
-//std::cout << "In_index " << std::hex << in_index << std::endl;
+    } else {
+        // random access
+        unsigned int randvalue=0x12345678; // initialize to non-zero value!
+    randvops1:
+        for (unsigned int count = 0; count < num_times; count++) {
+            #pragma HLS LOOP_TRIPCOUNT min = c_total max = c_total
+            //Per iteration of this loop perform vSize vector addition
+    randvops2:
+            for (unsigned int i = 0; i < vSize; i++) {
+                #pragma HLS LOOP_TRIPCOUNT min = VDATA_SIZE max = VDATA_SIZE
+                randvalue = next_lfsr32(randvalue); // 32 bits
+                // mask is less than vSize by construct, 
+                // so randvalueclipped will be less than vSize too
+                unsigned int randvalueclipped = randvalue & mask;
+                unsigned int candidate = in_index + randvalueclipped; // less than 2x vSize
+                // need to make sure it is strictly less than vSize
+                // note: don't need a true modulo operator because of the above masking and derived constraints
+                unsigned int in_index_random = candidate < vSize ? candidate : candidate-vSize;
 
-        out[in_index] = tmpOutAdd;
-      }
-    }
-    else {
-      //Per iteration of this loop perform vSize vector addition
-      Loop_seq: for (int i = 0; i < vSize; i++) {
-        #pragma HLS LOOP_TRIPCOUNT min = c_loop max = c_loop
-        in_index = i;
-        tmpIn1 = in1[in_index];
-        tmpIn2 = in2[in_index];
+                in_index = in_index_random;
 
-      vector_seq:
-        for (int k = 0; k < VDATA_SIZE; k++) {
-          tmpOutAdd.data[k] = tmpIn1.data[k] + tmpIn2.data[k];
+                v_dt tmpIn1 = in1[in_index];
+                v_dt tmpIn2 = in2[in_index];
+                v_dt tmpOutAdd;
+
+    randvops3: // will be unrolled because parent loop is pipelined
+                for (int k = 0; k < VDATA_SIZE; k++) {
+                    tmpOutAdd.data[k] = tmpIn1.data[k] + tmpIn2.data[k];
+                }
+
+                out[in_index] = tmpOutAdd;
+            }
         }
 
-        out[in_index] = tmpOutAdd;
-      }
     }
-  }
-}
 }
 
